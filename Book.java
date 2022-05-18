8e217ed61b17210900c5a76eba0e2e4e5ba212e3
JEEVANSH <60225293+JEEVANSH@users.noreply.github.com>
2022-05-18 19:34:50 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,30 @@
+package com.example.spring10step;
+
+public class Book {
+	long id;
+	String name;
+	String author;
+	
+	public Book(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+	
+	
+	
+
+}
